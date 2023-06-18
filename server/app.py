24795cbd2cd2a2fdf681015bc4154ef119c18c54
Matthew Phan <mttphan261@gmail.com>
2023-06-17 19:14:34 -0500

    first attempt done

--- server/app.py
+++ server/app.py
@@ -31,8 +31,94 @@ api = Api(app)
 
 @app.route("/")
 def home():
-    return ""
+    return "This is the home page"
 
+class Customers(Resource):
+    def get(self):
+        customers = [c.to_dict(only=('id', 'name', 'email')) for c in Customer.query.all()]
+        return make_response(customers, 200)
+
+api.add_resource(Customers, '/customers')
+
+class CustomerByID(Resource):
+    def get(self, id):
+        try:
+            customer = Customer.query.filter_by(id=id).first().to_dict()
+            return make_response(customer, 200)
+        except:
+            return {"error": "404: Customer not found"}, 404
+        
+api.add_resource(CustomerByID, '/customers/<int:id>')
+
+class NewCustomer(Resource):
+    def post(self):
+        data = request.get_json()
+        try:
+            new_customer = Customer(
+                name=data['name'],
+                email=data['email']
+            )
+            db.session.add(new_customer)
+            db.session.commit()
+            return make_response(new_customer.to_dict(), 201)
+        except:
+            return {"error": "400: Validation error"}, 400
+
+
+api.add_resource(NewCustomer, '/customers')
+
+class Locations(Resource):
+    def get(self):
+        locations = [l.to_dict(only=('id', 'name', 'max_party_size')) for l in Location.query.all()]
+        return make_response(locations, 200)
+
+api.add_resource(Locations, '/locations')        
+
+class LocationByID(Resource):
+    def delete(self, id):
+        try:
+            location = Location.query.filter_by(id=id).first()
+            db.session.delete(location)
+            db.session.commit()
+            return make_response({}, 400)
+        except:
+            return {"error": "404: Activity not found"}, 404
+
+api.add_resource(LocationByID, '/locations/<int:id>')
+
+class NewReservation(Resource):
+    def post(self):
+        data = request.get_json()
+        try:
+            new_reservation = Reservation(
+                # reservation_date = data['reservation_date'],
+                reservation_date=datetime.datetime.strptime(
+                    data.get("reservation_date"), "%Y-%m-%d"
+                ).date(),
+                customer_id = data['customer_id'],
+                location_id = data['location_id'],
+                party_size = data['party_size'],
+                party_name = data['party_name']
+            )
+            db.session.add(new_reservation)
+            db.session.commit()
+            return make_response(new_reservation.to_dict(), 201)
+        except:
+            return {"error": "400: Validation error"}, 400
+
+api.add_resource(NewReservation, '/reservations')
+
+class ReservationByID(Resource):
+    def delete(self, id):
+        try:
+            reservation = Reservation.query.filter_by(id=id).first()
+            db.session.delete(reservation)
+            db.session.commit()
+            return make_response({}, 400)
+        except:
+            return {"error": "404: Activity not found"}, 404
+
+api.add_resource(ReservationByID, '/reservations/<int:id>')
 
 if __name__ == "__main__":
     app.run(port=5555, debug=True)
--- server/models.py
+++ server/models.py
@@ -18,19 +18,117 @@ metadata = MetaData(naming_convention=convention)
 db = SQLAlchemy(metadata=metadata)
 
 
-class Customer:
+class Customer(db.Model, SerializerMixin):
     __tablename__ = "customers"
 
+
+
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable=False)
+    email=db.Column(db.String, nullable=False, unique=True)
 
+    reservations = db.relationship('Reservation', back_populates='customer')
+    locations = association_proxy('reservations', "location")
 
-class Location:
+    #SERIALS
+    serialize_rules = ('-reservations.customer',)
+
+    #VALIDATIONS
+    @validates('name')
+    def validate_name(self, key, name):
+        if not name:
+            raise ValueError('Customer name required')
+        return name
+    
+    @validates('email')
+    def validate_email(self, key, email):
+        if '@' not in email:
+            raise ValueError('Customer email required')
+        return email
+
+    def _repr__(self):
+        return f'<Customer {self.id}: {self.name}>'
+
+class Location(db.Model, SerializerMixin):
     __tablename__ = "locations"
 
+
+
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable=False)
+    max_party_size = db.Column(db.Integer, nullable=False)
 
+    reservations = db.relationship('Reservation', back_populates='location')
+    customers = association_proxy('reservations', "customer")
 
-class Reservation:
+    #SERIALS
+    serialize_rules = ('-reservations.location',)
+
+    #VALIDATIONS
+    @validates('name')
+    def validate_name(self, key, name):
+        if not name:
+            raise ValueError('Location name required')
+        return name
+    
+    @validates('max_party_size')
+    def validate_max_party_size(self, key, max_party_size):
+        if not max_party_size:
+            raise ValueError('Max party size required')
+        return max_party_size
+
+    def _repr__(self):
+        return f'<Location {self.id}: {self.name}>'
+
+class Reservation(db.Model, SerializerMixin):
     __tablename__ = "reservations"
 
+
+
     id = db.Column(db.Integer, primary_key=True)
+    party_name = db.Column(db.Integer, nullable=False)
+    party_size = db.Column(db.Integer)
+    reservation_date = db.Column(db.Date, nullable=False)
+
+    #ForeignKeys
+    location_id = db.Column(db.Integer, db.ForeignKey('locations.id'), nullable=False)
+    customer_id = db.Column(db.Integer, db.ForeignKey('customers.id'), nullable=False)
+
+    #relationships
+    customer = db.relationship('Customer', back_populates='reservations')
+    location = db.relationship('Location', back_populates='reservations')
+
+    serialize_rules = ('-customer.reservations', '-location.reservations',)
+
+    #VALIDATIONS
+    @validates('reservation_date')
+    def validate_date(self,key,reservation_date):
+        if not reservation_date or not isinstance(reservation_date, datetime.date):
+            raise TypeError('Reservation date required')
+        return reservation_date
+    
+    @validates('party_name')
+    def validate_party_name(self,key,party_name):
+        if not party_name or len(party_name) < 1:
+            raise ValueError('Reservation party name requried')
+        return party_name
+    
+    @validates('customer_id')
+    def validate_customer_id(self, key, customer_id):
+        if not customer_id or not isinstance(customer_id, int):
+            raise ValueError('Customer ID required')
+        return customer_id
+
+    @validates('location_id')
+    def validate_location_id(self, key, location_id):
+        if not location_id or not isinstance(location_id, int):
+            raise ValueError('Location ID required')
+        return location_id
+
+
+    def _repr__(self):
+        return f'<Reservation {self.id}>'
+
+    
+
+
